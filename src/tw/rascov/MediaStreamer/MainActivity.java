e6d2f926b66cdb8d06c16d9cc0d6279f256e99f7
Min-Cheng Chan <rascov@gmail.com>
2011-10-19 00:57:01 +0800

    first version of MediaStreamServer add permission: RECORD_AUDIO

--- src/tw/rascov/MediaStreamer/MainActivity.java
+++ src/tw/rascov/MediaStreamer/MainActivity.java
@@ -12,7 +12,11 @@ public class MainActivity extends Activity {
 	EditText editText1, editText2;
 	Button button1;
 	TextView textView1;
-	
+	String ip;
+	int port;
+    MediaStreamServer mss;
+    MediaStreamClient msc;
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,19 +27,28 @@ public class MainActivity extends Activity {
         editText2 = (EditText) findViewById(R.id.editText2);
         button1 = (Button) findViewById(R.id.button1);
         textView1 = (TextView) findViewById(R.id.textView1);
-        
-        final String ip = editText1.getText().toString();
-        final int port = Integer.valueOf(editText2.getText().toString());
+        ip = editText1.getText().toString();
+        port = Integer.valueOf(editText2.getText().toString());
         
         button1.setOnClickListener(new OnClickListener() {		
 			@Override
 			public void onClick(View v) {
-				if(ip.equals("127.0.0.1")) {
-					new MediaStreamServer(port);
-					new MediaStreamClient(ip, port);
+				if(button1.getText().toString().equals("Start")) {
+					button1.setText("Stop");
+					if(ip.equals("127.0.0.1") || ip.equals("0.0.0.0")) {
+						mss = new MediaStreamServer(port);
+					}
+					/*
+					if(!ip.equals("0.0.0.0")) {
+						msc = new MediaStreamClient(ip, port);
+					}
+					*/
 				}
-				if(ip.equals("0.0.0.0")) new MediaStreamServer(port);
-				else new MediaStreamClient(ip, port);
+				else if(button1.getText().toString().equals("Stop")) {
+					button1.setText("Start");
+					mss.stop();
+					//msc.stop();
+				}
 			}
 		});
         
--- src/tw/rascov/MediaStreamer/MediaStreamServer.java
+++ src/tw/rascov/MediaStreamer/MediaStreamServer.java
@@ -1,7 +1,47 @@
 package tw.rascov.MediaStreamer;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+
 public class MediaStreamServer {
-	public MediaStreamServer(int port) {
-		
+	static final int frequency = 44100;
+    static final int channelConfiguration = AudioFormat.CHANNEL_CONFIGURATION_MONO;
+    static final int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
+    boolean isRecording;
+    int recBufSize;
+    ServerSocket sockfd;
+    Socket connfd;
+	AudioRecord audioRecord;
+	
+	public MediaStreamServer(final int port) {
+		recBufSize = AudioRecord.getMinBufferSize(frequency, channelConfiguration, audioEncoding);
+		audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, frequency, channelConfiguration, audioEncoding, recBufSize);
+
+		new Thread() {
+			byte[] buffer = new byte[recBufSize];
+			public void run() {
+				try {
+					sockfd = new ServerSocket(port);
+					connfd = sockfd.accept();
+				}
+				catch (IOException e) { e.printStackTrace(); }
+		        audioRecord.startRecording();
+		        isRecording = true;
+		        while (isRecording) {  
+		            int readSize = audioRecord.read(buffer, 0, recBufSize);
+		            try { connfd.getOutputStream().write(buffer, 0, readSize); }
+		            catch (IOException e) { e.printStackTrace(); }
+		        }  
+		        audioRecord.stop();
+			}
+		}.start();
+	}
+	
+	public void stop() {
+		isRecording = false;
 	}
 }
