0706eb029f76ddafade6cba6f58553af0324dea1
rascov <rascov@gmail.com>
2011-10-19 19:55:52 +0800

    branch for udp version

--- src/tw/rascov/MediaStreamer/MediaStreamClient.java
+++ src/tw/rascov/MediaStreamer/MediaStreamClient.java
@@ -1,5 +1,8 @@
 package tw.rascov.MediaStreamer;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.Socket;
 
 import android.content.Context;
@@ -14,7 +17,8 @@ public class MediaStreamClient {
     static final int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
     boolean isPlaying;
 	int playBufSize;
-    Socket connfd;
+    //Socket connfd;
+	DatagramSocket udpsock;
 	AudioTrack audioTrack;
 	
 	public MediaStreamClient(final Context ctx, final String ip, final int port) {
@@ -22,9 +26,27 @@ public class MediaStreamClient {
 		audioTrack = new AudioTrack(AudioManager.STREAM_VOICE_CALL, frequency, channelConfiguration, audioEncoding, playBufSize, AudioTrack.MODE_STREAM);
 		audioTrack.setStereoVolume(1f, 1f);
 		
+		InetAddress peerip;
+		try {
+			udpsock = new DatagramSocket();
+			
+			peerip = InetAddress.getByName(ip);
+	    	DatagramPacket sendPacket = new DatagramPacket(new byte[1], 1, peerip, port);
+			udpsock.send(sendPacket);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			Intent intent = new Intent()
+				.setAction("tw.rascov.MediaStreamer.ERROR")
+				.putExtra("msg", e.toString());
+			ctx.sendBroadcast(intent);
+			return;
+		}
+    	
 		new Thread() {
 			byte[] buffer = new byte[playBufSize];
 			public void run() {
+				/*
 				try { connfd = new Socket(ip, port); }
 				catch (Exception e) {
 					e.printStackTrace();
@@ -34,11 +56,17 @@ public class MediaStreamClient {
 					ctx.sendBroadcast(intent);
 					return;
 				}
+				*/
 				audioTrack.play();
 				isPlaying = true;
 		        while (isPlaying) {
 		        	int readSize = 0;
-		            try { readSize = connfd.getInputStream().read(buffer); }
+		            //try { readSize = connfd.getInputStream().read(buffer); }
+		        	DatagramPacket receivePacket;
+		        	try {
+		        		receivePacket = new DatagramPacket(buffer, buffer.length);
+						udpsock.receive(receivePacket);
+		        	}
 		            catch (Exception e) {
 	            		e.printStackTrace();
 						Intent intent = new Intent()
@@ -47,10 +75,12 @@ public class MediaStreamClient {
 						ctx.sendBroadcast(intent);
 						break;
 					}
-		        	audioTrack.write(buffer, 0, readSize);
+		        	//audioTrack.write(buffer, 0, readSize);
+		        	audioTrack.write(receivePacket.getData(), 0, receivePacket.getData().length);
 		        }  
 		        audioTrack.stop();
-				try { connfd.close(); }
+				//try { connfd.close(); }
+		        try { udpsock.close(); }
 				catch (Exception e) { e.printStackTrace(); }
 			}
 		}.start();
--- src/tw/rascov/MediaStreamer/MediaStreamServer.java
+++ src/tw/rascov/MediaStreamer/MediaStreamServer.java
@@ -1,5 +1,9 @@
 package tw.rascov.MediaStreamer;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,15 +19,16 @@ public class MediaStreamServer {
     static final int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
     boolean isRecording;
     int recBufSize;
-    ServerSocket sockfd;
-    Socket connfd;
+    //ServerSocket sockfd;
+    //Socket connfd;
+    DatagramSocket udpsock;
 	AudioRecord audioRecord;
 	
 	public MediaStreamServer(final Context ctx, final int port) {
 		recBufSize = AudioRecord.getMinBufferSize(frequency, channelConfiguration, audioEncoding);
 		audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, frequency, channelConfiguration, audioEncoding, recBufSize);
 		
-		try { sockfd = new ServerSocket(port); }
+		try { udpsock = new DatagramSocket(port); }
 		catch (Exception e) {
 			e.printStackTrace();
 			Intent intent = new Intent()
@@ -33,9 +38,22 @@ public class MediaStreamServer {
 			return;
 		}
 		
+		/*
+		try { sockfd = new ServerSocket(port); }
+		catch (Exception e) {
+			e.printStackTrace();
+			Intent intent = new Intent()
+				.setAction("tw.rascov.MediaStreamer.ERROR")
+				.putExtra("msg", e.toString());
+			ctx.sendBroadcast(intent);
+			return;
+		}
+		*/
+		
 		new Thread() {
 			byte[] buffer = new byte[recBufSize];
 			public void run() {
+				/*
 				try { connfd = sockfd.accept(); }
 				catch (Exception e) {
 					e.printStackTrace();
@@ -45,11 +63,29 @@ public class MediaStreamServer {
 					ctx.sendBroadcast(intent);
 					return;
 				}
+				*/
+				DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
+				try { udpsock.receive(receivePacket); }
+				catch (Exception e) {
+					e.printStackTrace();
+					Intent intent = new Intent()
+						.setAction("tw.rascov.MediaStreamer.ERROR")
+						.putExtra("msg", e.toString());
+					ctx.sendBroadcast(intent);
+					return;
+				}
+				InetAddress peerip = receivePacket.getAddress();
+                int peerport = receivePacket.getPort();
+                
 		        audioRecord.startRecording();
 		        isRecording = true;
-		        while (isRecording) {  
+		        while (isRecording && peerip!=null) {  
 		            int readSize = audioRecord.read(buffer, 0, recBufSize);
-		            try { connfd.getOutputStream().write(buffer, 0, readSize); }
+		            try {
+		            	//connfd.getOutputStream().write(buffer, 0, readSize);
+		            	DatagramPacket sendPacket = new DatagramPacket(buffer, readSize, peerip, peerport);
+		                udpsock.send(sendPacket);
+		            }
 		            catch (Exception e) {
 						e.printStackTrace();
 						Intent intent = new Intent()
@@ -60,7 +96,8 @@ public class MediaStreamServer {
 					}
 		        }  
 		        audioRecord.stop();
-				try { connfd.close(); }
+				//try { connfd.close(); }
+		        try { udpsock.close(); }
 				catch (Exception e) { e.printStackTrace(); }
 			}
 		}.start();
@@ -68,7 +105,9 @@ public class MediaStreamServer {
 	
 	public void stop() {
 		isRecording = false;
+		/*
 		try { sockfd.close(); }
 		catch (Exception e) { e.printStackTrace(); }
+		*/
 	}
 }
